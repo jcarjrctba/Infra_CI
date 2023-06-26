bda80385404e6f8d5bfca8af0b0f2ebb9339c42f
Junior <114120831+jcarjrctba@users.noreply.github.com>
2023-06-26 16:34:15 -0300

    Update Backend.tf

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-jcarjrctba"
-    # key    = "Prod/terraform.tfstate"
+    key    = "Prod/terraform.tfstate"
     region = "us-west-2"
   }
 }
